e1812ce39f2006808ab2980c3f511f4e992f9801
wildun19 <wildanabdunnaseer19@gmail.com>
2025-02-08 17:59:56 +0700

    about new

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -25,16 +25,66 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="container">
-        <div class="row d-flex align-items-center">
-            <div class="col-lg-6 col-md-6 col-sm-12">
-                <img src="img/index.jpg" alt="Profile Image" class="img-fluid">
+        <div class="row align-items-center">
+            <div class="col-lg-6 py-3 wow fadeInUp ">
+                <div class="img-fluid py-3 text-center">
+                    <img src="img/index.jpg" alt="Profile Image" width="" height="300">
+                </div>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="col-lg-6 py-3 wow fadeInRight">
                 <div class="container">
-                    <p>Velreact Lab adalah tim profesional yang mengembangkan website modern dan responsif. Kami menggunakan Laravel sebagai backend yang kuat, serta Tailwind CSS, Bootstrap, dan JavaScript untuk antarmuka yang elegan dan fungsional.</p>
-                    <p>Didirikan oleh tiga ahli di bidangnya, tim kami terdiri dari lulusan Teknologi Informasi dengan pengalaman luas dalam merancang solusi digital yang efisien, aman, dan skalabel. Velreact Lab berkomitmen untuk mengubah ide Anda menjadi produk web berkualitas tinggi.</p>
+                    <p>Velreact Lab is a team of professionals specializing in the development of modern and
+                        responsive
+                        websites. We leverage Laravel as a robust backend framework, combined with Tailwind CSS,
+                        Bootstrap, and JavaScript on the frontend to create elegant and functional user interfaces.
+                    </p>
+                    <p>Founded by three experts in the field, our team consists of Information Technology graduates
+                        with
+                        extensive experience in designing efficient, secure, and scalable digital solutions.
+                        Velreact
+                        Lab is committed to transforming your ideas into innovative, high-quality web products.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-6 py-3 wow fadeInRight">
+                <div class="container">
+                    <p>Velreact Lab is a team of professionals specializing in the development of modern and
+                        responsive
+                        websites. We leverage Laravel as a robust backend framework, combined with Tailwind CSS,
+                        Bootstrap, and JavaScript on the frontend to create elegant and functional user interfaces.
+                    </p>
+                    <p>Founded by three experts in the field, our team consists of Information Technology graduates
+                        with
+                        extensive experience in designing efficient, secure, and scalable digital solutions.
+                        Velreact
+                        Lab is committed to transforming your ideas into innovative, high-quality web products.</p>
+                </div>
+            </div>
+            <div class="col-lg-6 py-3 wow fadeInUp ">
+                <div class="img-fluid py-3 text-center">
+                    <img src="img/index.jpg" alt="Profile Image" width="" height="300">
+                </div>
+            </div>
+        </div>
+        <div class="row align-items-center">
+            <div class="col-lg-6 py-3 wow fadeInUp ">
+                <div class="img-fluid py-3 text-center">
+                    <img src="img/index.jpg" alt="Profile Image" width="" height="300">
+                </div>
+            </div>
+            <div class="col-lg-6 py-3 wow fadeInRight">
+                <div class="container">
+                    <p>Velreact Lab is a team of professionals specializing in the development of modern and
+                        responsive
+                        websites. We leverage Laravel as a robust backend framework, combined with Tailwind CSS,
+                        Bootstrap, and JavaScript on the frontend to create elegant and functional user interfaces.
+                    </p>
+                    <p>Founded by three experts in the field, our team consists of Information Technology graduates
+                        with
+                        extensive experience in designing efficient, secure, and scalable digital solutions.
+                        Velreact
+                        Lab is committed to transforming your ideas into innovative, high-quality web products.</p>
                 </div>
             </div>
         </div>
