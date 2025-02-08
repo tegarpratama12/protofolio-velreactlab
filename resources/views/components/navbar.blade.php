57a817794b21cf15e77087947b03dd63dd26fe0f
Morshinka <tegarprarama461@gmail.com>
2025-02-08 13:43:55 +0700

    chage color navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,14 +1,11 @@
-<nav class=" navbar navbar-expand-lg navbar-dark bg-primary py-3">
-    <a class="navbar-brand ms-4" href="#">
-        <img src="img/study_Words_1.png" width="50" height="30" alt="">
-    </a>
+<nav class=" navbar navbar-expand-lg navbar-light py-3" style="background-color: #FBF5E5">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="container collapse navbar-collapse" id="navbarNav">
         {{-- chage font size to fs-5 --}}
-        <ul class="navbar-nav ms-auto fs-5 fw-bold">
+        <ul class="navbar-nav ms-auto fs-5 fw-semibold">
             <li class="nav-item active">
                 <a class=" nav-link" href="#">Home</a>
             </li>
