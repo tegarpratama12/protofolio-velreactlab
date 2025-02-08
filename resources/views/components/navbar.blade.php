1a31b4394f82758316a07f4ea314eb96ceb40601
Morshinka <tegarprarama461@gmail.com>
2025-02-08 13:30:05 +0700

    navbar_container

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,10 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-3">
     <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+        aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="container collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
             <li class="nav-item active">
                 <a class="nav-link" href="#">Home</a>
@@ -14,9 +14,6 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled">Disabled</a>
             </li>
         </ul>
     </div>
