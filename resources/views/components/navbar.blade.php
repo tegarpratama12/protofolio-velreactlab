e7d5a126d8a522433b0461b10ad78bbb8c523523
Morshinka <tegarprarama461@gmail.com>
2025-02-08 13:38:52 +0700

    adding logo

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,12 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary py-3">
-    <a class="navbar-brand" href="#">Navbar</a>
+<nav class=" navbar navbar-expand-lg navbar-dark bg-primary py-3">
+    <a class="navbar-brand ms-4" href="#">
+        <img src="img/study_Words_1.png" width="50" height="30" alt="">
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
         aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="container collapse navbar-collapse" id="navbarNav">
         {{-- chage font size to fs-5 --}}
-        <ul class="navbar-nav ms-auto fs-5">
+        <ul class="navbar-nav ms-auto fs-5 fw-bold">
             <li class="nav-item active">
                 <a class=" nav-link" href="#">Home</a>
             </li>
